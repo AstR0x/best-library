cdf11eff2c46e7411a5679c0671f89e1788512c7
Vlad Gumerov <vesel4ack98@mail.ru>
2018-12-09 17:26:58 +0300

    added JSDoc comments

--- js/best-library.js
+++ js/best-library.js
@@ -1,3 +1,11 @@
+//функция возвращающая рандомное число
+/**
+ * get random number
+ * @param {Number} max upper bound
+ * @param {Number} min bottom bound
+ * @param {Number} wrongNumber a number that should not appear
+ * @return {Number} random number
+ * */
 function random(max, min = 0, wrongNumber) {
   if (max <= min) {
     throw new Error('max <= min');
@@ -11,27 +19,37 @@ function random(max, min = 0, wrongNumber) {
   }
   return randomNumber;
 }
-
-function arrayOfRandomUniqueNumber(amount, max, min = 0, uniq = false) {
-  if (amount > max - min && uniq == true) {
+/**
+ * get array of random numbers
+ * @param {Number} amount amount of array elements
+ * @param {Number} max upper bound
+ * @param {Number} min bottom bound
+ * @param {Boolean} unique unique numbers
+ * @return {Array} array of random numbers
+ */
+function arrayOfRandomUniqueNumbers(amount, max, min = 0, unique = false) {
+  if (amount > max - min && unique == true) {
     throw new Error('amount more than maximum - minimum');
   }
-  let arrayOfRandomUniqueNumber = [];
+  let arrayOfRandomUniqueNumbers = [];
   let randomNumber;
 
   for (let i = 0; i < amount;) {
     randomNumber = random(max, min);
-    if (arrayOfRandomUniqueNumber.indexOf(randomNumber) !== -1 && uniq == true) {
+    if (arrayOfRandomUniqueNumbers.indexOf(randomNumber) !== -1 && unique == true) {
       continue;
     }
-    arrayOfRandomUniqueNumber[i] = randomNumber;
+    arrayOfRandomUniqueNumbers[i] = randomNumber;
 
     i++;
   }
-  return arrayOfRandomUniqueNumber;
+  return arrayOfRandomUniqueNumbers;
 }
-
-
+/**
+ * sort array of numbers
+ * @param {Array} array array that need sort
+ * @return {Array} sorted array
+ */
 function sortNumbers(array) {
   array.sort((a, b) => {
     if (a > b) return 1;
@@ -40,25 +58,27 @@ function sortNumbers(array) {
   return array;
 }
 
-//функция создания массива
-function newArray(amount, type) {
+/**
+ * create new array
+ * @param {Number} amount amount of new array
+ * @param {Number} elem element that need to fill the array
+ * @return {Array} new array
+ */
+function newArray(amount, elem) {
   let array = [];
-  console.log(type);
   for(var i = 0; i < amount; i++) {
-    array.push(type);
+    array.push(elem);
   }
   return array;
 }
 
-//Получение последнего элемента массива
-function getLastElement(array) {
+/**
+ * get last element of array
+ * @param {Array} array
+ * @return {*}
+ */
+function lastElement(array) {
   return array[array.length - 1];
 }
 
 
-
-
-
-// function sort() {
-//
-// }
