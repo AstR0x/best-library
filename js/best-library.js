345ff1aa8579bfc3d26a800a91be9225971e0c59
Vlad Gumerov <vesel4ack98@mail.ru>
2018-12-14 19:10:44 +0300

    changed the parameters of the randNum and randArr functions

--- js/best-library.js
+++ js/best-library.js
@@ -1,85 +1,128 @@
 /**
- * get random number
- * @param {Number} max upper bound
- * @param {Number} min bottom bound
- * @param {Number} wrongNumber a number that should not appear
- * @return {Number} random number
- * */
-function random(max, min = 0, wrongNumber) {
-  if (max <= min) {
-    throw new Error('max <= min');
+   * get random number
+   * @param {Number} x bottom bound
+   * @param {Number} y  upper bound
+   * @param {Number} z a number that should not appear
+   * @return {Number} random number
+   * */
+function randNum(x, y, z = undefined) {
+  if (arguments.length === 1) {
+    return Math.round(Math.random() * x);
   }
-  let randomNumber = Math.round((Math.random() * (max - min) + min));
-  if(!wrongNumber) {
-    return randomNumber;
+  let randomNumber = Math.round((Math.random() * (y - x) + x));
+  if (y < x) {
+    throw new Error('maximum less than minimum');
   }
-  while(randomNumber === wrongNumber) {
-    randomNumber = Math.round((Math.random() * (max - min) + min));
+  while(randomNumber === z) {
+    randomNumber = Math.round((Math.random() * (y - x) + x));
   }
   return randomNumber;
 }
 
-/**
- * get array of random numbers
- * @param {Number} amount amount of array elements
- * @param {Number} max upper bound
- * @param {Number} min bottom bound
- * @param {Boolean} unique unique numbers
- * @return {Array} array of random numbers
- */
-function arrayRandom(amount, max, min = 0, unique = false) {
-  if (amount > max - min && unique == true) {
-    throw new Error('amount more than maximum - minimum');
-  }
-  let arrayOfRandomUniqueNumbers = [];
-  let randomNumber;
-
-  for (let i = 0; i < amount;) {
-    randomNumber = random(max, min);
-    if (arrayOfRandomUniqueNumbers.indexOf(randomNumber) !== -1 && unique == true) {
-      continue;
+  /**
+   * get array of random numbers
+   * @param {Number} amount amount of array elements
+   * @param {Number} max upper bound
+   * @param {Number} min bottom bound
+   * @param {Boolean} unique unique numbers
+   * @return {Array} array of random numbers
+   */
+  function randArr(amount, min, max, unique = false) {
+    if (amount > max - min && unique == true) {
+      throw new Error('amount more than difference of maximum and minimum');
     }
-    arrayOfRandomUniqueNumbers[i] = randomNumber;
+    let arrOfRUN = [];
+    let randomNumber;
 
-    i++;
+    for (let i = 0; i < amount;) {
+      randomNumber = randNum(min, max);
+      if (arrOfRUN.indexOf(randomNumber) !== -1 && unique == true) {
+        continue;
+      }
+      arrOfRUN[i] = randomNumber;
+      i++;
+    }
+    return arrOfRUN;
   }
-  return arrayOfRandomUniqueNumbers;
-}
 
-/**
- * sort array of numbers
- * @param {Array} array array that need sort
- * @return {Array} sorted array
- */
-function sortArray(array) {
-  array.sort((a, b) => {
-    if (a > b) return 1;
-    if (a < b) return -1;
-  });
-  return array;
-}
-
-/**
- * create new array
- * @param {Number} amount amount of new array
- * @param {Number} elem element that need to fill the array
- * @return {Array} new array
- */
-function newArray(amount, elem) {
-  let array = [];
-  for(var i = 0; i < amount; i++) {
-    array.push(elem);
+  /**
+   * sort array of numbers
+   * @param {Array} array array that need sort
+   */
+  function sortArr(array) {
+    array.sort((a, b) => {
+      if (a > b) return 1;
+      if (a < b) return -1;
+    });
   }
-  return array;
-}
+
+  /**
+   * create new array
+   * @param {Number} amount amount of new array
+   * @param {Number} elem element that need to fill the array
+   * @return {Array} new array
+   */
+  function newArr(amount, elem) {
+    let array = [];
+    for(var i = 0; i < amount; i++) {
+      array.push(elem);
+    }
+    return array;
+  }
+
+  /**
+   * get last element of array
+   * @param {Array} array
+   * @return {*}
+   */
+  function lastElem(array) {
+    return array[array.length - 1];
+  }
 
 /**
- * get last element of array
- * @param {Array} array
- * @return {*}
+ * perform an operation on two arrays
+ * @param {Array} arr1 array that will change
+ * @param {Array} arr2 second array
+ * @param {String} oper operation that will be performed
  */
-function lastElement(array) {
-  return array[array.length - 1];
-}
+function arrOperations(arr1, arr2, oper) {
+    switch (oper) {
+      case "+":
+        for(let i = 0; i < arr1.length; i++) {
+          arr1[i] += arr2[i];
+         }
+        break;
+
+      case "-":
+        for(let i = 0; i < arr1.length; i++) {
+          arr1[i] -= arr2[i];
+        }
+        break;
+
+      case "*":
+        for(let i = 0; i < arr1.length; i++) {
+          arr1[i] *= arr2[i];
+        }
+        break;
+
+      case "/":
+        for(let i = 0; i < arr1.length; i++) {
+          arr1[i] /= arr2[i];
+        }
+        break;
+
+      default:
+        throw new Error('wrong operation');
+    }
+  }
+
+
+
+console.log(randNum(0, 10));
+console.log(randArr(5, 0, 50));
+
+
+
+
 
 
--- test/js/test.js
+++ test/js/test.js
@@ -3,17 +3,17 @@ var assert = chai.assert;
 
 describe("randNum", function() {
   it('return type is an number', function() {
-    assert.isNumber(randNum(100, 0))
+    assert.isNumber(randNum(0, 100))
   });
 
   it('returned number not equal wrong number ', function() {
     for(let i = 0; i < 50; i++) {
-      assert.notEqual(randNum(10, 0, 5), 5)
+      assert.notEqual(randNum(0, 10, 5), 5)
     }
   });
 
   it("returned value in the range from minimum to maximum", function() {
-    let randomNumber = randNum(100, 0);
+    let randomNumber = randNum(0, 100);
     assert.isBelow(randomNumber, 101);
     assert.isAbove(randomNumber, -1);
   });
@@ -21,11 +21,11 @@ describe("randNum", function() {
 
 describe("randArr", function() {
   it('return type is an array', function() {
-    assert.isArray(randArr(10, 5, 0));
+    assert.isArray(randArr(10, 0, 5));
   });
 
   it('all numbers unique', function() {
-    let array = randArr(100, 100, 0, true);
+    let array = randArr(100, 0, 100, true);
     for(let i = 0; i < 99; i++) {
       for(let j = i + 1; j < 100; j++) {
         assert.notEqual(array[i],array[j]);
@@ -34,7 +34,7 @@ describe("randArr", function() {
   });
 
   it("returned value in the range from minimum to maximum", function() {
-    let array = randArr(100, 100, 0, false);
+    let array = randArr(100, 0, 100, false);
     array.forEach(function (elem) {
       assert.isBelow(elem, 101);
       assert.isAbove(elem, -1);
@@ -45,7 +45,7 @@ describe("randArr", function() {
 
 describe('sortArr', function () {
   it('array sorted', function () {
-    let array = randArr(50, 50, 0, false);
+    let array = randArr(50, 0, 50, false);
     for(let i = 1; i < 50; i++) {
       array[i] >= array[i - 1];
     }
